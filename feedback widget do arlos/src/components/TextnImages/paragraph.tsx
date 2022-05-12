2f059e09397b5a2963cd0c233d5ca7349601c10b
karl <88538786+Karl-developer@users.noreply.github.com>
2022-05-11 22:45:06 -0300

    Mais alterações

--- feedback widget do arlos/src/components/TextnImages/paragraph.tsx
+++ feedback widget do arlos/src/components/TextnImages/paragraph.tsx
@@ -1,10 +1,9 @@
 export function MainProse(){
     return(
-        <div className="ml-[146px] xl:ml-[123px] lg:ml-[97px] md:ml-[70px] sm:ml-[46px] xs:ml-[10px] font-Heebo text-4xl xl:text-3xl lg:text-2xl md:text-xln sm:text-lg xs:text-sm mt-12 text-black dark:text-white">
-            <p>Nossa empresa tem o objetivo de <br/>levar até o espaço <br/> de 
-                maneira bem diferentes <br/>com base no web development,
-                <br/> somos a socketreach,<br/>uma empresa determinada <br/> a fazer você
-                contar todas as estrelas
+        <div className="ml-[146px] xl:ml-[103px] lg:ml-[97px] md:ml-[70px] sm:ml-[46px] xs:ml-[10px] font-Heebo text-4xl xl:text-3xl lg:text-2xl md:text-xln sm:text-lg xs:text-sm mt-12 text-black dark:text-white">
+            <p>Nossa empresa tem o objetivo de <br/>mostrar novos horizontes,<br/>
+                mostrar lugares jamais vistos<br/>usando programação.
+                <br/>Não é magia, nem só tecnologia,<br/>é <strong>astronomia</strong>, e também um pouco de I.A
             </p>
         </div>
     )
--- feedback widget do arlos/tailwind.config.js
+++ feedback widget do arlos/tailwind.config.js
@@ -47,4 +47,7 @@ module.exports = {
     require('tailwind-scrollbar'),
     // ...
   ],
+  variants: {
+    scrollbar: ['rounded']
+}
 }
