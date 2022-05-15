129b687ccff26bd0f4012d7cd9a679894a474936
karl <88538786+Karl-developer@users.noreply.github.com>
2022-05-15 00:12:55 -0300

    fiz ums negócinhos legais aki

--- feedback widget do arlos/src/components/Contato/Popover.tsx
+++ feedback widget do arlos/src/components/Contato/Popover.tsx
@@ -5,7 +5,7 @@ import { Elements } from './PopoverElements'
 export function PopContato(){
     return(
         <Popover>
-            <Popover.Button className='bg-black dark:bg-brand-300 p-2 rounded-full'>
+            <Popover.Button className='bg-black dark:bg-brand-300 p-2 rounded-full '>
             <ArrowsOut size={32} />
             </Popover.Button>
             <Popover.Panel>
--- feedback widget do arlos/src/components/Contato/PopoverElements.tsx
+++ feedback widget do arlos/src/components/Contato/PopoverElements.tsx
@@ -1,5 +1,24 @@
+import UseDarkMode from "../ToggleButton/UseDarkMode"
+
 export function Elements(){
-    return(
-        <div>bla bla bla</div>
-    )
-}
+    const [colorTheme, setTheme] = UseDarkMode();
+  
+    return (
+      <div>
+        <button>
+          <span
+            className="transition duration-200"
+            onClick={() => setTheme(colorTheme)}
+          >
+            {colorTheme === "dark" ? (
+             <div className="transition-opacity duration-100 bg-amber-300 p-4 mt-4 rounded-lg text-black text-left">De dia colocamos novas features
+             <br /> e analisamos pull requests <br /> nossas e de outros usuários.</div>
+            ) : (
+                <div className="text-left mt-4 rounded-lg bg-indigo-800 p-4 ">De noite lemos as issues <br /> do github
+                e corrigimos bugs.</div>
+            )}
+          </span>
+        </button>
+      </div>
+    );
+  }
